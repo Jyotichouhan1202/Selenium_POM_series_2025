a733cc55abef1839cb225afe5485b99f0888db91
jyoti <jyotichouhan1202@gmail.com>
2025-09-12 19:22:50 +0530

    Change in code

--- src/main/java/Com/qa/openkart/pages/Demo.java
+++ src/main/java/Com/qa/openkart/pages/Demo.java
@@ -5,8 +5,9 @@ public class Demo {
 	public static void main(String[] args) {
 		int a= 10;
 		int b=20;
-		int c= a+b;
-		System.out.println(c);
+		int c=40;
+		int d= a+b+c;
+		System.out.println(d);
 
 	}
 

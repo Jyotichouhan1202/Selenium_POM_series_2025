abcc9511115275ff0af49c45335b5e3558460d0f
jyoti <jyotichouhan1202@gmail.com>
2025-09-13 19:19:58 +0530

    new code added

--- src/main/java/Com/qa/openkart/pages/Demo.java
+++ src/main/java/Com/qa/openkart/pages/Demo.java
@@ -1,13 +1,14 @@
 package Com.qa.openkart.pages;
 
 public class Demo {
-
+	
 	public static void main(String[] args) {
 		int a= 10;
 		int b=20;
 		int c=40;
-		int d= a+b+c;
-		System.out.println(d);
+		int d= 10;
+		int e= a+b+c+d;
+		System.out.println(e);
 
 	}
 
